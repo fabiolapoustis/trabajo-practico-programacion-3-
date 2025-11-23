0a5988a6b85b0df0fa030b40d351005b52482ccc
Julian-Ayuso <jnayuso@hotmail.com>
2025-11-23 16:48:47 -0300

    bd

---
+++ sq.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+//import { Sequelize, DataTypes } from "sequelize";
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+//conexion bbdd
+
+const sequelize = new Sequelize("PG","root","",{
+    host: "localhost",
+    dialect: "mysql",
+    port: PORT,
+})
+
+module.exports = { sequelize, DataTypes};
+
+//Crear tabla usuario
+
+const Usuario = sequelize.define("Usuario", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  }
+});
+
+app.get("/", async (req, res) => {
+    try{
+        const users = await Usuario.findAll({});
+
+        res.send(users);
+    }catch(error){
+        console.log({error})
+    }
+});
+
+sequelize
+  .sync({ force: true }) // force -> true/false   alter -> true/false
+  .then(() => {
+    console.log("Conectando a la data base");
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("El servidor esta corriendo en el puerto " + PORT);
+    });
+  })
+  .catch((error) => console.log({ error }));
