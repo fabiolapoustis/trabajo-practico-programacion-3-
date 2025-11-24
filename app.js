bd3d2d35a17ef210acdc3e12921e38be6e56e600
fabiolapoustis <fabiolapoustis@gmail.com>
2025-11-23 22:52:38 -0300

    se agrega la pagina cargar productos

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const multer = require('multer');
 
-
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'vistas'));
 
@@ -13,6 +14,40 @@ app.get('/inicioadmin', (req, res) => {
 });
 
 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+
+const upload = multer({ storage: storage });
+
+
+app.get('/cargarproducto', (req, res) => {
+    res.render('cargarproducto');
+});
+
+
+
+app.post('/cargarproducto', upload.single('imagen'), (req, res) => {
+    const { titulo, categoria, peso, precio } = req.body;
+    const imagen = req.file.filename;
+    
+    console.log({
+        titulo,
+        categoria,
+        peso,
+        precio,
+        imagen
+    });
+    
+    res.send("Producto cargado correctamente");
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
